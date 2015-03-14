2dda023a7531d6b85f4940547e8bcea5bc4383e3
jhermann <jh@web.de>
2015-03-14 20:52:10 +0100

    :new: imported py-generic-project template

---
+++ src/gh-commander/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=bad-whitespace
+"""
+    gh-commander – GitHub Commander is a tool to access the GitHub APIv3
+    from the CLI and automate otherwise tedious tasks.
+
+    Copyright ©  2015 Jürgen Hermann <jh@web.de>
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+__url__             = "https://github.com/jhermann/gh-commander"
+__version__         = "0.1.0"
+__license__         = "Apache 2.0"
+__author__          = "Jürgen Hermann"
+__author_email__    = "jh@web.de"
+__keywords__        = "hosted.by.github github cli tool automation"
+
+__all__ = []
