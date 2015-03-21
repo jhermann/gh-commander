6868b9168fea40fcac74fce287d5c4b19c226e98
jhermann <jh@web.de>
2015-03-21 03:13:16 +0100

    :arrow_upper_right: added --version option

--- src/gh_commander/__main__.py
+++ src/gh_commander/__main__.py
@@ -15,8 +15,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import absolute_import
+
+import sys
 
 import click
+
+from . import __version__
+
 
 __app_name__ = 'gh'
 CONTEXT_SETTINGS = dict(
@@ -25,6 +31,9 @@ CONTEXT_SETTINGS = dict(
 
 
 @click.group(context_settings=CONTEXT_SETTINGS)
+@click.version_option(#version=__version__, prog_name=__app_name__,
+    message='%(prog)s %(version)s [Python {}]'.format(' '.join(sys.version.split())),
+)
 @click.option('-q', '--quiet', is_flag=True, default=False, help='Be quiet (show only errors).')
 @click.option('-v', '--verbose', is_flag=True, default=False, help='Create extra verbose output.')
 def cli(quiet=False, verbose=False): # pylint: disable=unused-argument
@@ -40,4 +49,5 @@ def help():
 
 
 if __name__ == "__main__": # imported via "python -m"?
-    cli()
+    __package__ = 'gh_commander' # pylint: disable=redefined-builtin
+    cli() # pylint: disable=no-value-for-parameter
